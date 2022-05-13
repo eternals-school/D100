5e856541fb6ad00cc886b8e4a3b9d19bf82dca80
“Muni” <you@example.com>
2022-05-13 18:26:12 +0530

    pwa

---
+++ Day_1/Munis/myScript.js
@@ -0,0 +1,24 @@
+// function clickMe() {
+//     var headerDom = document.getElementById('header1');
+//     headerDom.innerText = "muni"
+// };
+// // document.getElementById("clickk").addEventListener("click", function(){
+// // document.getElementById("header1").innerHTML="muni";
+// // }
+// // );
+// var myReq = new XMLHttpRequest();
+// myReq.open("GET", "://github.com")
+// myReq.addEventListener('load', function (res) {
+//     if (myReq.readyState == 4)
+//         console.log(myReq.responseText)
+
+// })
+// myReq.send()
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("clickk").addEventListener("click", function () {
+        document.getElementById("header1").innerHTML = "muniss";
+    }
+    );
+
+});
